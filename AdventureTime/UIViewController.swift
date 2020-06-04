8f77d32d2ead224669f8de8c07f2d1ff256a27e0
Roshan Pai <mpai@wisc.edu>
2020-06-04 14:31:59 -0500

    AdventureTime App mostly done

---
+++ AdventureTime/UIViewController.swift
@@ -0,0 +1,23 @@
+//
+//  UIViewController.swift
+//  AdventureTime
+//
+//  Created by mpai on 6/2/20.
+//  Copyright © 2020 Intuit. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIViewController {
+    
+    func showAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .destructive, handler: nil)
+        alert.addAction(action)
+        DispatchQueue.main.async {
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
+}
+
